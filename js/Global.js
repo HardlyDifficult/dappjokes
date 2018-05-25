1ddb8aa22b67fec858fa1749e4c38fc8907f4d71
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2018-05-25 16:43:51 -0700

    Switch to mainnet.

--- js/Global.js
+++ js/Global.js
@@ -1,7 +1,7 @@
 // Global variables used by our Dapp
 var contract_address;
 
-var is_mainnet = false;
+var is_mainnet = true;
 var nebulas_chain_id, nebulas_domain; 
 var gas_price = 1000000;
 var gas_limit = 200000;
@@ -10,6 +10,7 @@ if(is_mainnet)
 {
     nebulas_chain_id = 1;
     nebulas_domain = "https://mainnet.nebulas.io";
+    contract_address = "n1zEiv8MXzCc58aKXe3vxzyqYZ4JcQCY17L";
 } 
 else 
 {
